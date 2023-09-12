7a8bd10b442019ed172f32880dbdec8cd333d023
Mariia-Ospn <126498309+Mariia-Ospn@users.noreply.github.com>
2023-09-12 22:31:00 +0300

    task4 "TransactionHistory" done

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,21 @@
+import { TransactionItem } from "./TransactionItem";
+import PropTypes from 'prop-types';
+import css from './transaction.module.css';
+
+export const TransactionHistory = ({ items }) => (
+    <table className={css['transaction-history']}>
+    <thead>
+      <tr>
+        <th>Type</th>
+        <th>Amount</th>
+        <th>Currency</th>
+      </tr>
+    </thead>
+
+    <tbody>{items.map(TransactionItem)}</tbody>
+  </table>
+);
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.object),
+};
