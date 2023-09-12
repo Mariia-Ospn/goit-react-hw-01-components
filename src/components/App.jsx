6e0e93d30ea7d6f5bf4e6ce05b3401c3c7018403
Mariia-Ospn <126498309+Mariia-Ospn@users.noreply.github.com>
2023-09-12 21:21:32 +0300

    task2 "Statistics" done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import data from './data/data.json'
 import user from './data/user.json'
-import {Profile} from 'components/Profile/Profile'
+import { Statistics } from './Statistics/Statistics';
+import {Profile} from './Profile/Profile'
 
 export const App = () => {
   return (
     <>
     <Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+    />
+    <Statistics title="Upload stats" stats={data} />
+    <Statistics stats={data} />  
 </>
   );
 };
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import { StatisticItem } from './StatisticItem';
+import css from './statistics.module.css';
+
+export const Statistics = ({ title, stats }) => (
+  <section className={css.statistics}>
+    {title && <h2 className={css.title}>{title}</h2>}
+    <ul className={css['stat-list']}>{stats.map(StatisticItem)}</ul>
+  </section>
+);
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.array,
+};
