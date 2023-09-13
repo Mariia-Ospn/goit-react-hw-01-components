aa3d7d8ada4e611762d6a5d4b57d1377cf9519d9
Mariia-Ospn <126498309+Mariia-Ospn@users.noreply.github.com>
2023-09-13 10:24:42 +0300

    add propTypes at all lists

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -28,5 +28,12 @@ export const TransactionHistory = ({ items }) => (
 );
 
 TransactionHistory.propTypes = {
-  items: PropTypes.arrayOf(PropTypes.object),
+    items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
